92bfde635906c7f717f68c4629f1299af33a9bd9
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-13 14:54:05 -0500

    Updated from Brython Server: 11/13/2017 2:54:04 PM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,14 @@
+#Greg Phillips
+#11/13/17
+#middleword.py
+
+words = input("Enter words: ").split(" ") #.split(" ") recogmizes spaces and splits them into lists
+num = len(words[:])
+
+middle = num/2
+
+if middle%2 == 0:
+    print(words[middle-1], words[middle])
+else:
+        print(words[middle])
 
