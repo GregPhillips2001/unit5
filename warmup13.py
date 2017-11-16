2b3163f272f4ba323e9595a8a036d3045fd067f7
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-16 14:16:56 -0500

    Updated from Brython Server: 11/16/2017 2:16:56 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,13 @@
+#Greg Phillips
+#11/16/17
+#warmup13.py
 
+from random import randint
+
+for i in range(0,20):
+    list = [i]
+    list.append(i)
+    list.sort()
+    print(0)
+    print(-1)
+    
