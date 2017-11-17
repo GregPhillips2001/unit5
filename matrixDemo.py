be1ecebb19eb7ff7c7528302c735a42eee37e211
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-17 14:29:55 -0500

    Updated from Brython Server: 11/17/2017 2:29:53 PM

--- matrixDemo.py
+++ matrixDemo.py
@@ -2,10 +2,11 @@
 #11/17/17
 #matrixDemo.py
 
-board = [["a","b","c"],['d','e','f'],['g','h','i']]
-
 def printBoard():
     for row in range(0,3):
         for col in range(0,3):
             print(board[row][col],' ',end = '')
         print()
+
+board = [["a","b","c"],['d','e','f'],['g','h','i']]
+printBoard()
