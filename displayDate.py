c4dbab40a524473a2ac3fbd89788d12363815154
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-20 14:54:11 -0500

    Updated from Brython Server: 11/20/2017 2:54:10 PM

--- displayDate.py
+++ displayDate.py
@@ -1,1 +1,16 @@
+#Greg Phillips
+#11/20/17
+#displayDate.py
+
+import datetime
+
+today = datetime.date.today()
+day = today.weekday()
+
+print(day,today.month,today.day,today.year)
+
+wordDay = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+wordMonth = 
+
+
 
