05a18be6be7fe93b2b6e01bc2156ae3dfa552948
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-20 14:28:24 -0500

    Updated from Brython Server: 11/20/2017 2:28:23 PM

--- antsDemo.py
+++ antsDemo.py
@@ -13,9 +13,11 @@ if __name__ == "__main__":
     red = Color(0xff0000,1)
     ant = CircleAsset(10,LineStyle(1,red),red)
     
+    data = {}
+    data['antList'] = []
     
-    for i in range(100):
-        Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT)))
+    for i in range(10):
+        data['antList'].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
     
     App().run()
     
