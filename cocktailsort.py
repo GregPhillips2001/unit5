ae3c1d338b07bdf7e94a4c5539fac9563cd7f4db
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-27 14:40:19 -0500

    Updated from Brython Server: 11/27/2017 2:40:19 PM

--- cocktailsort.py
+++ cocktailsort.py
@@ -1,15 +1,22 @@
 #greg phillips
 #11/27/17
-#cocktailsort.py
+#cocktailsort.py - implementation of cocktail sort
 
 from random import randint
 from time import time
 
 N = 100 #how many numbers will be sorted
 
-def mySort(L):
-    #put your code here
-    return L
+def mySort(A):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(0,len(A)-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i] #swap in python
+                swapped = True
+        
+    return A
 
 if __name__ == '__main__':
     
