dad4f28b43202f1dcaa90a4bce03abf63ea04b0e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-04 14:24:35 -0500

    Updated from Brython Server: 12/4/2017 2:24:34 PM

--- quiz5.py
+++ quiz5.py
@@ -8,3 +8,9 @@ def rand5():
     from random import randint
     print([randint(1,100),randint(1,100),randint(1,100),randint(1,100),randint(1,100)])
 rand5()
+
+#lastElement
+
+def lastElement(x = ['house','bed','couch']):
+    print(x[-1])
+lastElement()
