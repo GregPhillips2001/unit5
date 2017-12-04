e4ad707db3a352fe7e09f608d392260f01c0590b
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-04 14:22:12 -0500

    Updated from Brython Server: 12/4/2017 2:22:11 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,11 @@
+#Greg Phillips
+#12/4/17
+#quiz5.py
 
+#rand5
+
+def rand5():
+    from random import randint
+    print([randint(1,100),randint(1,100),randint(1,100),randint(1,100),randint(1,100)])
+    
+rand5()
